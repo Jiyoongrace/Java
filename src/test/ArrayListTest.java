8b683d456bd082c32e55dbc9b02aad9e1874e0c5
Jiyoongrace <giyun3632@naver.com>
2024-04-03 12:00:25 +0900

    [FEAT] ArrayList

--- src/test/ArrayListTest.java
+++ src/test/ArrayListTest.java
@@ -1,6 +1,7 @@
 package test;
 
 import java.util.ArrayList;
+import java.util.List;
 
 class MyStudent {
 
@@ -8,7 +9,12 @@ class MyStudent {
 
 public class ArrayListTest {
     public static void main(String[] args) {
+
         // ArrayList 객체를 만들어서 사용하자.
-        ArrayList list = new ArrayList();
+        // List 계열은 객체면 type에 상관없이 다 저장 가능하다.
+        // 다른 reference data type들을 저장할 수 있다.
+        // 그런데 실제로 구현하다보면 거의 예외없이
+        // 같은 데이터 타입을 이용해서 사용하게 된다. (마치 배열처럼)
+        List<String> list = new ArrayList<String>();
     }
 }
